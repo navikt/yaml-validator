88abf29fe3bef2f83c531e81ee23656261b1f7c2
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-01-24 12:53:07 +0100

    style: add emojis to log statements

--- validator.py
+++ validator.py
@@ -55,7 +55,7 @@ def evaluate_yaml_schema(schema_path, document_path):
     document = __load_yaml_document(document_path)
     valid = validator.validate(document, schema)
     if not valid:
-        logging.error(f"Failed to validate document '{document_path}' against schema `{schema_path}`")
+        logging.error(f"❌\tFailed to validate document '{document_path}' against schema `{schema_path}`")
         pp = pprint.PrettyPrinter(width=200)
         logging.error(pp.pformat(validator.errors))
         print('')
@@ -66,20 +66,20 @@ def validate_yaml_for_dir(schema_path, document_dir_path):
     results = [(document_path.relative_to('./'), evaluate_yaml_schema(schema_path, document_path.relative_to('./')))
                for document_path in pathlib.Path(document_dir_path).glob('**/*.yml')]
     if any(not is_valid for (_, is_valid) in results):
-        logging.error(f"Found validation failures for the following document(s) in '{document_dir_path}':")
+        logging.error(f"❌\tFound validation failures for the following document(s) in '{document_dir_path}':")
         for (path, is_valid) in results:
             if not is_valid:
-                logging.error(f'{path}')
+                logging.error(f'❌\t{path}')
         sys.exit(1)
     else:
-        logging.info(f"All documents in '{document_dir_path}' are valid!")
+        logging.info(f"✅\tAll documents in '{document_dir_path}' are valid!")
 
 
 def validate_single_yaml(schema_path, document_path):
     if not evaluate_yaml_schema(schema_path, document_path):
         sys.exit(1)
     else:
-        logging.info(f"Document '{document_path}' is valid!")
+        logging.info(f"✅\tDocument '{document_path}' is valid!")
 
 
 def main():
